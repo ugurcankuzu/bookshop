5741bda79de5b7faeabca6a5e7c6782a718db7cb
ugurcankuzu <uci2001@hotmail.com>
2023-11-05 14:29:24 +0100

    seperate route.js file created and called from index.js

--- book-store-api/index.js
+++ book-store-api/index.js
@@ -4,17 +4,13 @@ require("dotenv").config();
 const firebaseApp = require("./firebase");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const {
-  getFirestore,
-  collection,
-  doc,
-  getDocs,
-} = require("firebase/firestore");
+const router = require("./router");
 //initializations
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use("/api",router);
 
 app.listen(process.env.PORT, () => {
   console.log(`Listening on ${process.env.PORT}`);
