6c54bb0ff1525f5a7f2d1330e4153e7ec5bb9d43
ugurcankuzu <uci2001@hotmail.com>
2023-11-12 22:35:51 +0100

    Campaign Slider has moved to new ui.

--- book-store/src/components/home/slider.tsx
+++ book-store/src/components/home/slider.tsx
@@ -7,14 +7,55 @@ import image4 from "../../../public/Desktop - 4.jpg";
 import Image from "next/image";
 import "swiper/css";
 import "swiper/css/navigation";
+import "swiper/css/pagination";
 
-import { Navigation } from "swiper/modules";
+import { Navigation, Pagination } from "swiper/modules";
+import getLinkByLabel from "@/util/getLinkByLabel";
 
 export default function CampaignSlider() {
   const campaignImages = [image1, image2, image3, image4];
   return (
     <section className={campaignSliderStyles.sectionFrame}>
-      <Swiper
+      <div className={campaignSliderStyles.sectionWrapper}>
+        <div className={campaignSliderStyles.swiperWrapper}>
+          <Swiper
+            pagination={{
+              type: "progressbar",
+            }}
+            modules={[Navigation, Pagination]}
+            navigation={true}
+            className={campaignSliderStyles.swiperObject}
+          >
+            {campaignImages &&
+              campaignImages.map((campaignImage, index) => (
+                <SwiperSlide key={index}>
+                  <Image
+                    src={campaignImage}
+                    alt="Campaign Images"
+                    priority
+                    className={campaignSliderStyles.campaignImages}
+                  />
+                </SwiperSlide>
+              ))}
+          </Swiper>
+        </div>
+        <div className={campaignSliderStyles.campaignTitleWrapper}>
+          <p className={campaignSliderStyles.campaignTitle}>
+            Easiest way to find your book!
+          </p>
+          <p className={campaignSliderStyles.campaignSubTitle}>
+            Fastest way to find & buy books Online. Not cheapest but the best...
+          </p>
+          {getLinkByLabel("Shop", campaignSliderStyles.shopButton)}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+/**
+ * 
+ * <Swiper
         modules={[Navigation]}
         navigation={true}
         className={campaignSliderStyles.swiperObject}
@@ -31,12 +72,18 @@ export default function CampaignSlider() {
             </SwiperSlide>
           ))}
       </Swiper>
-    </section>
-  );
-}
-
+ */
 const campaignSliderStyles = {
-  sectionFrame: "w-full h-fit max-h-[500px] flex justify-center items-center my-8",
-  swiperObject: "[--swiper-navigation-color:black] w-full h-full shadow-xl xl:w-4/5",
+  sectionFrame: "w-full flex justify-center items-center p-2",
+  sectionWrapper:
+    "w-full flex flex-col sm:flex-row justify-start sm:justify-center items-center gap-8 lg:w-4/5 2xl:w-3/5",
+  swiperWrapper: "w-full sm:w-3/5 sm:order-2 shadow-xl",
+  swiperObject:
+    "[--swiper-navigation-color:black] [--swiper-pagination-color:black]",
   campaignImages: "w-full max-h-[500px] object-center object-cover",
+  campaignTitleWrapper: "w-full flex flex-col items-start justify-center gap-4",
+  campaignTitle: "text-3xl lg:text-[36px] font-extrabold text-coal/90",
+  campaignSubTitle: "text-lg text-coal/40",
+  shopButton:
+    "w-1/2 border-2 border-coal/90 px-2 py-1 text-coal/90 font-bold text-lg text-center",
 };
