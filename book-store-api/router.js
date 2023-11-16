4b12737e29bc457ff3458da1957500945289ff6e
ugurcankuzu <uci2001@hotmail.com>
2023-11-16 17:30:02 +0100

    getProductByName route is ready.

--- book-store-api/router.js
+++ book-store-api/router.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const getAllCategories = require("./routeHandlers/categories/getAllCategories");
 const getAllProducts = require("./routeHandlers/products/getAllProducts");
+const getProductByName = require("./routeHandlers/products/getProductByName");
 const getProductsBySalecount = require("./routeHandlers/products/getProductsBySalecount");
 router.get("/allProducts", getAllProducts);
 router.get("/products", getProductsBySalecount);
 router.get("/allCategories", getAllCategories);
+router.get("/products/:productName", getProductByName);
 module.exports = router;
