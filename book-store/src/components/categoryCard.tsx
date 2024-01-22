0ab341c63940257150d75a95b5cf6060a79f3a8b
ugurcankuzu <uci2001@hotmail.com>
2024-01-22 20:25:56 +0100

    some hover

--- book-store/src/components/categoryCard.tsx
+++ book-store/src/components/categoryCard.tsx
@@ -1,9 +1,9 @@
 import TCategory from "@/types/category";
-import Link from "next/link";
-import bgVertical from "../../public/lib-portrait.jpeg";
-import bgHorizontal from "../../public/lib-landscape.jpeg";
-import getLinkByLabel from "@/util/getLinkByLabel";
 import Route from "@/types/route";
+import getLinkByLabel from "@/util/getLinkByLabel";
+import Link from "next/link";
+import { motion } from "framer-motion";
+import bgVertical from "../../public/lib-portrait.jpeg";
 interface ICategoryCard {
   category: TCategory;
   indexNumber: number;
@@ -19,7 +19,9 @@ export default function CategoryCard({
     <>
       {!horizontalLayout && (
         <Link
-          href={ShopRoute.path + "?category=" + encodeURI(category.categoryName)}
+          href={
+            ShopRoute.path + "?category=" + encodeURI(category.categoryName)
+          }
           style={{
             backgroundImage: `url(${bgVertical.src})`,
           }}
@@ -35,7 +37,9 @@ export default function CategoryCard({
       )}
       {horizontalLayout && (
         <Link
-          href={ShopRoute.path + "?category=" + encodeURI(category.categoryName)}
+          href={
+            ShopRoute.path + "?category=" + encodeURI(category.categoryName)
+          }
           className={categoryCardStyle.horizontalLayout.categoryCard}
         >
           {category.categoryName}
@@ -48,10 +52,10 @@ export default function CategoryCard({
 const categoryCardStyle = {
   gridLayout: {
     categoryCard:
-      "w-full border border-black bg-center bg-fixed bg-auto backdrop-opacity-10 shadow-xl",
+      "w-full border border-black bg-center bg-fixed bg-auto backdrop-opacity-10 shadow-xl relative after:content-[''] after:w-0 after:h-1/2 hover:after:bg-pearl after:absolute after:left-0 after:top-0 before:content-[''] before:w-0 before:h-1/2 before:absolute before:bottom-0 before:right-0 hover:before:bg-pearl before:transition-all after:transition-all after:duration-[1s] before:duration-[1s] before:ease-in-out hover:after:w-full hover:before:w-full ",
     bigPicture: " col-span-2 row-span-2 sm:order-first",
     categoryName:
-      "w-full h-full flex justify-center items-center bg-coal/90 text-3xl text-pearl font-bold",
+      "w-full h-full flex justify-center items-center bg-coal/90 text-3xl text-pearl font-bold absolute z-[100]",
   },
   horizontalLayout: {
     categoryCard: "bg-coal/90 text-pearl px-2 py-1 rounded shadow snap-center",
