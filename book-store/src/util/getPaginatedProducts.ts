bbb9038fec366d950d38c162eb1b9ad38e6678ef
ugurcankuzu <uci2001@hotmail.com>
2024-02-06 11:51:25 +0100

    Undefined 404 response handled in Empty Category results

--- book-store/src/util/getPaginatedProducts.ts
+++ book-store/src/util/getPaginatedProducts.ts
@@ -1,14 +1,15 @@
-export default async function getPaginatedProducts(currentUrl:string) {
+export default async function getPaginatedProducts(currentUrl: string) {
   const URLSearch = new URLSearchParams(currentUrl);
   const page = URLSearch.get("page");
   const category = URLSearch.get("category");
-  const query = `?page=${page ?? 1}${category ? "&category="+category:""}`
+  const query = `?page=${page ?? 1}${category ? "&category=" + category : ""}`;
   const response = await fetch(
-    process.env.NEXT_PUBLIC_API_URL+"/getPaginatedProducts"+query,
+    process.env.NEXT_PUBLIC_API_URL + "/getPaginatedProducts" + query,
     { method: "GET" }
   );
   if (response.status === 200) {
     const result = await response.json();
     return result;
   }
+  return [];
 }
